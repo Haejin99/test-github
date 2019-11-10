0b3f29150b553982c19305b5b6dcc80fd3d745f4
Haejin <poporysky@naver.com>
2019-11-10 13:26:02 +0900

    add sum function

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,10 @@ package com.mycompany.test;
 
 public class Calculator {
     public static void main(String[] args) {
-        
+
     }
+}
+
+private static int sum(int a, int b) {
+    return a + b;
 }
